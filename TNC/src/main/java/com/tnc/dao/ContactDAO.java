6832a1598fc58cbc0b8cdd0f6d642b541ea9ca83
BEER <thanarat.rt@gmail.com>
2014-08-31 10:51:30 +0700

    initial TNC project

---
+++ TNC/src/main/java/com/tnc/dao/ContactDAO.java
@@ -0,0 +1,14 @@
+package com.tnc.dao;
+
+import java.util.List;
+
+import com.tnc.domain.Contact;
+
+public interface ContactDAO {
+
+	public void addContact(Contact contact);
+
+	public List<Contact> listContact();
+
+	public void removeContact(Integer id);
+}
---
+++ TNC/src/main/java/com/tnc/service/ContactService.java
@@ -0,0 +1,14 @@
+package com.tnc.service;
+
+import java.util.List;
+
+import com.tnc.domain.Contact;
+
+public interface ContactService {
+
+	public void addContact(Contact contact);
+
+	public List<Contact> listContact();
+
+	public void removeContact(Integer id);
+}
