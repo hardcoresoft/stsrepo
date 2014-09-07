73f6fa6c231bd0f3ee9872f5b58451a54f6276f7
thanasit <thanasitthetprasit@Thanasits-MacBook-Air.local>
2014-09-07 14:23:36 +0700

    Send mail controller

---
+++ TNC/src/main/java/com/tnc/controller/SendEmailController.java
@@ -0,0 +1,45 @@
+package com.tnc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tnc.utils.StringUtils;
+ 
+@Controller
+@RequestMapping("/sendEmail.do")
+public class SendEmailController {
+ 
+    @Autowired
+    private JavaMailSender mailSender;
+     
+    @RequestMapping(method = RequestMethod.POST)
+    public String doSendEmail(HttpServletRequest request) {
+        // takes input from e-mail form
+        String recipientAddress = request.getParameter("recipient");
+        String subject = request.getParameter("subject");
+        String message = request.getParameter("message");
+         
+        // prints debug info
+        System.out.println("To: " + recipientAddress);
+        System.out.println("Subject: " + StringUtils.getThai(subject, "UTF-8", "UTF-8"));
+        System.out.println("Message: " + StringUtils.getThai(message, "UTF-8", "UTF-8"));
+         
+        // creates a simple e-mail object
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(recipientAddress);
+        email.setSubject(subject);
+        email.setText(message);
+         
+        // sends the e-mail
+        mailSender.send(email);
+         
+        // forwards to the view named "Result"
+        return "/mail/Result";
+    }
+}
